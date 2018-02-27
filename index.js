f9e56afdde316f682a95cd08a1395232fa752838
Deinyefa Eporwei <df.eporwei@gmail.com>
2018-02-27 18:27:54 -0500

    app header

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { AppRegistry, View } from 'react-native';
+import React from 'react';
+// import App from './App';
+
+// Components
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
 
 AppRegistry.registerComponent('albums', () => App);
